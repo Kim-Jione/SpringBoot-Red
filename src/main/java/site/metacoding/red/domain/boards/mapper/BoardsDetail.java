7722485ab02eb1125394c44cb3b1bdbbea7ee573
kimjiwon <hghg1036@naver.com>
2022-09-02 12:54:06 +0900

    board join 완료

---
+++ src/main/java/site/metacoding/red/domain/boards/mapper/BoardsDetail.java
@@ -0,0 +1,14 @@
+package site.metacoding.red.domain.boards.mapper;
+
+import java.sql.Timestamp;
+
+public class BoardsDetail {
+	private Integer id;
+	private String title;
+	private String content;
+	private Timestamp createdAt;
+	private String userId;
+	private String username;
+	private String password;
+	private String email;
+}
